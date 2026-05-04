c6ef475c81d2dce0dc0748abe4bca061523d571f
Lyheng <lyhengtep@gmail.com>
2026-05-04 23:59:44 +0700

    chore: merge alembic heads

---
+++ alembic/versions/37c7d2604498_merge_heads.py
@@ -0,0 +1,26 @@
+"""merge heads
+
+Revision ID: 37c7d2604498
+Revises: 8e3022903015, m5n6o7p8q9r0
+Create Date: 2026-05-04 23:59:30.919581
+"""
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+# revision identifiers, used by Alembic.
+revision = '37c7d2604498'
+down_revision = ('8e3022903015', 'm5n6o7p8q9r0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
